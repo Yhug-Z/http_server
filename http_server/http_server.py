ff68174138b57308299ab995e646876c1fa5f57f
Yhug-Z <aahappyaa@hotmail.com>
2019-08-16 10:24:02 +0800

    http server start

---
+++ http_server/http_server.py
@@ -0,0 +1,18 @@
+"""
+httpserver v3.0
+
+获取http请求
+解析http请求
+将请求发送给WebFrame
+从WebFrame接收反馈数据
+将数据组织为Response格式发送给客户端
+"""
+from socket import *
+
+
+class HTTPServer:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
